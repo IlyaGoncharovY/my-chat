31d13a63f6cf8239b4613507e07d54cb17dcb2f8
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-01-22 19:36:43 +0300

    fix (App) added the ability to reconect, App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Messages} from "./messages/Messages";
 import {AddMessagesForm} from "./addMessageForm/addMessagesForm";
 
-
-export const wsChannel = new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx")
 
 export type ChatMessageType = {
     userId: number,
@@ -15,10 +13,38 @@ export type ChatMessageType = {
 
 function App() {
 
+    const [wsChannel, setWsChannel] = useState<WebSocket | null>(null)
+
+
+    useEffect(() => {
+        let ws: WebSocket
+        const closeHandler = () => {
+            setTimeout(createChannel, 3000)
+        }
+
+        function createChannel() {
+
+            ws?.removeEventListener("close", closeHandler)
+            ws?.close()
+
+            ws = new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx")
+            ws.addEventListener("close", closeHandler)
+            setWsChannel(ws)
+        }
+
+        createChannel()
+
+        return () => {
+            ws.removeEventListener("close", closeHandler)
+            ws.close()
+        }
+    }, [])
+
+
     return (
         <div>
-            <Messages/>
-            <AddMessagesForm/>
+            <Messages wsChannel={wsChannel}/>
+            <AddMessagesForm wsChannel={wsChannel}/>
         </div>
     )
 }
