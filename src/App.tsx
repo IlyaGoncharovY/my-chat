9e9180f497a38fbcc61094abf9450c0f68cce693
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-01-22 17:53:05 +0300

    fix (App) render new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+import {Messages} from "./messages/Messages";
+import {AddMessagesForm} from "./addMessageForm/addMessagesForm";
+
+
+export const wsChannel = new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx")
+
+export type ChatMessageType = {
+    userId: number,
+    userName: string,
+    message: string,
+    photo: string
+}
 
 function App() {
-  return (
-      <div>
 
-      </div>
-  )
+    return (
+        <div>
+            <Messages/>
+            <AddMessagesForm/>
+        </div>
+    )
 }
 
 export default App;
