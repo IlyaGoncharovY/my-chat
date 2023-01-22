85467a05a5fdedf320ffddf8e1e65f3e34649b97
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-01-22 17:51:35 +0300

    feat (Message.tsx) add component Message.tsx

---
+++ src/messages/message/Message.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {ChatMessageType} from "../../App";
+
+type MessageType = {
+   message: ChatMessageType
+}
+
+export const Message = (props: MessageType) => {
+    return (
+        <div>
+            <div>
+                <img src={props.message.photo} style={{width: "40px"}} alt={"avatar"}/>
+            </div>
+            <br/>
+            <span>
+                {props.message.userName}
+            </span>
+            <div>
+                <b>{props.message.message}</b>
+            </div>
+            <hr/>
+        </div>
+    );
+};
+
