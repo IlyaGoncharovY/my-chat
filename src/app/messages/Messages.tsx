d9911545ceea19c74ebc3d4eccd3145b98a9738d
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-02-05 16:53:13 +0300

    fix (messages) refactor code Messages.tsx. isLoggedIn

--- src/app/messages/Messages.tsx
+++ src/app/messages/Messages.tsx
@@ -1,11 +1,15 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {Message} from './message/Message';
 import {useAppSelector} from "../../utils/hook/hook";
+import {AddMessagesForm} from "./addMessageForm/addMessagesForm";
+import {Navigate} from "react-router-dom";
+import {PATH} from "../../utils/path/path";
 
 export const Messages = React.memo(() => {
 
     const messages = useAppSelector(state => state.Chat.messages)
     const messagesAnchorRef = useRef<HTMLDivElement>(null)
+    const isLoggedIn = useAppSelector(state => state.Auth.isLoggedIn)
 
     const [autoScrollActive, setAutoScrollActive] = useState(true)
 
@@ -19,16 +23,29 @@ export const Messages = React.memo(() => {
     }
 
     useEffect(() => {
+        if (!isLoggedIn) {
+            return;
+        }
         if (autoScrollActive) {
             messagesAnchorRef.current?.scrollIntoView({behavior: "smooth"})
         }
-    }, [messages])
+        return () => {
+            setAutoScrollActive(false)
+        }
+    }, [isLoggedIn, messages])
+
+    if (!isLoggedIn) {
+        return <Navigate to={PATH.LOGIN}/>
+    }
 
     return (
-        <div style={{height: "400px", overflowY: "auto"}} onScroll={scrollHandler}>
-            {messages.map((el) => <Message message={el} key={el.id}/>)}
-            <div ref={messagesAnchorRef}></div>
-        </div>
+        <>
+            <div style={{height: "400px", overflowY: "auto"}} onScroll={scrollHandler}>
+                {messages.map((el) => <Message message={el} key={el.id}/>)}
+                <div ref={messagesAnchorRef}></div>
+            </div>
+            <AddMessagesForm/>
+        </>
     );
 });
 
