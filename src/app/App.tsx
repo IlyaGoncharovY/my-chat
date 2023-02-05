1f5ea4c0391a791f7883c6df75e2946ec20d59ca
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-02-05 16:51:26 +0300

    fix (APP) refactor code for rrd, logout, initialized

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,47 @@
 import React, {useEffect} from 'react';
 import {Messages} from "./messages/Messages";
-import {AddMessagesForm} from "./addMessageForm/addMessagesForm";
 import {useAppDispatch, useAppSelector} from "../utils/hook/hook";
 import {startMessagesListening, stopMessagesListening} from "../bll/reducers/chat-reducer";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {LoginForm} from "./login/LoginForm";
+import {PATH} from "../utils/path/path";
+import {initializedAppTC} from "../bll/reducers/app-reducer";
+import {logOutTC} from "../bll/reducers/auth-reducer";
+
 
 function App() {
 
     const status = useAppSelector(state => state.Chat.status)
+    const isInitialized = useAppSelector(state => state.App.isInitialized)
 
     const dispatch = useAppDispatch()
 
+    const LogOutHandler = () => {
+        dispatch(logOutTC())
+    }
+
     useEffect(() => {
+        dispatch(initializedAppTC())
         dispatch(startMessagesListening())
         return () => {
             dispatch(stopMessagesListening())
         }
     }, [dispatch])
 
+    if (!isInitialized) {
+        return <div>...Loading</div>
+    }
+
     return (
         <div>
             {status === "error" ? <div>Please refresh page!</div> :
                 <>
-                    <Messages/>
-                    <AddMessagesForm/>
+                    <button onClick={LogOutHandler}>LogOut</button>
+                    <Routes>
+                        <Route path={PATH.LOGIN} element={<LoginForm/>}/>
+                        <Route path={PATH.MAIN} element={<Navigate to={PATH.CHAT_PAGE}/>}/>
+                        <Route path={PATH.CHAT_PAGE} element={<Messages/>}/>
+                    </Routes>
                 </>
             }
         </div>
